61e29e4adeeab7ee5fabe503009bada7d060eafa
bezara florent <bezaraflorent@gmail.com>
2024-03-08 23:08:57 +0300

    row = 30

--- resources/views/livewire/admin/universite/organigramme.blade.php
+++ resources/views/livewire/admin/universite/organigramme.blade.php
@@ -68,7 +68,7 @@
                         <label class="form-label">Contenus</label>
                         <div>
                             <div style="height: 100%;">
-                                <textarea class="form-control" rows="10" wire:model="body"></textarea>
+                                <textarea class="form-control" rows="30" wire:model="body"></textarea>
                                 {{-- <livewire:quill-text-editor wire:model.defer="body" theme="snow" /> --}}
                             </div>
                         </div>
